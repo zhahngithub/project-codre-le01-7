4c167a517fa6d210fdb0bd63f7201c312bef9e54
zhahngithub <63442511+zhahngithub@users.noreply.github.com>
2025-06-01 16:42:02 +0700

    initial sol

---
+++ Calculator/src/solution/Main.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+public class Main {
+    static CalculatorFrame frame;
+    public static void main(String[] args) {
+        frame=new CalculatorFrame("Calculator",1400,1000);
+        frame.setBackground(Color.decode("#32322F"));
+        frame.setVisible(true);
+        
+        
+
+    } 
+}
